42f25197103634a1d9faad3d29ec6175fec9356b
thinh-hoang <thinhngan99@gmail.com>
2022-10-10 18:00:38 +0700

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          Learn React
+          Learn hehe
         </div>
       </header>
     </div>
