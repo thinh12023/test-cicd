7b896e14d06604e28efa1481a812ffbeab282735
thinh-hoang <thinhngan99@gmail.com>
2022-10-11 02:29:31 +0700

    hehe

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          Learn hehe
+          Learn dm Giau
         </div>
       </header>
     </div>
